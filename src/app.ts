f76567e7a2dc917496978f9e92c028f284276f17
vedantSSJ <81308923+supersaiyangodSS@users.noreply.github.com>
2024-01-26 17:11:50 +0530

    add api route

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import express , { Request, Response } from 'express';
 import connectDB from './config/database.js';
+import main from './router/main.js';
 
 const app = express();
 connectDB();
+
+app.use('/api', main)
 
 export default app;
