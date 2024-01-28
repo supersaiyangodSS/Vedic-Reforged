add81c19aa754e2c08cf7922707eac9bb0eb05fb
vedantSSJ <81308923+supersaiyangodSS@users.noreply.github.com>
2024-01-28 23:34:00 +0530

    add uploader route

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import express , { Request, Response, Express } from 'express';
 import connectDB from './config/database.js';
 import main from './router/main.js';
 import multer from 'multer';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
 import path from 'path';
 import Sahity from './models/sahity.js';
 
@@ -11,8 +13,16 @@ connectDB();
 app.use(express.static('public'));
 app.use(express.json());
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
+
+const uploadPage = join(__dirname, '../public/upload.html');
+app.get('/', (req: Request, res: Response) => {
+    res.sendFile(uploadPage);
+})
 
 app.post('/upload', upload.single('jsonFile'), async (req: Request, res: Response) => {
     try {
