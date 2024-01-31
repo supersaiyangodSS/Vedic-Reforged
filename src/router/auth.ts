4b7c8edc55c5764e80f8a19ee8c45ffbbd7b1087
vedantSSJ <81308923+supersaiyangodSS@users.noreply.github.com>
2024-01-31 22:33:42 +0530

    add logout endpoint

--- src/router/auth.ts
+++ src/router/auth.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
-import { login } from '../controller/authController.js';
+import { login, logout } from '../controller/authController.js';
 
 const router = Router();
 
 router.post('/', login);
+router.get('/logout', logout);
 
 export default router;
