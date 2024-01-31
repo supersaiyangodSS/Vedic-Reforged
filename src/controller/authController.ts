c502625a68b4fddc67973c61ac2b2b2a908b52b8
vedantSSJ <81308923+supersaiyangodSS@users.noreply.github.com>
2024-01-31 22:36:00 +0530

    add logout controller

--- src/controller/authController.ts
+++ src/controller/authController.ts
@@ -2,28 +2,37 @@ import { Request, Response } from "express";
 import Admin from "../models/admin.js";
 import { compare } from 'bcrypt';
 
+
 const login = async (req: Request, res: Response) => {
     try {
-        const { username, password } = req.body;
-
+        const { username, password, remember } = req.body;
         if (!username || !password) {
             return res.status(403).json({ error: 'Username and password are required' });
         }
-
         const adminLogged = await Admin.findOne({ username });
-
         if (!adminLogged || !(await compare(password, adminLogged.password))) {
             return res.status(403).json({ error: 'Wrong username or password' });
         }
-
-        req.session.user = username;
         console.log(`User logged in as: ${req.session.user}`);
-
-        return res.status(302).json({ success: `Logged in as: ${req.session.user}` });
+        if (remember) {
+            req.session.cookie.maxAge = 30 * 24 * 60 * 60 * 1000;
+        }
+        req.session.user = username;
+        res.status(302).json({ success: `Logged in as: ${req.session.user}` });
     } catch (error) {
         console.error(error);
-        res.status(500).json({ error: 'Internal Server Error' });
+        return res.status(500).json({ error: 'Internal Server Error' });
     }
 };
 
-export { login };
+const logout = async (req: Request, res: Response) => {
+    req.session.destroy((err) => {
+        if(err) {
+            console.log(err);
+            return res.json({ error: `Error destroying session: ${err}` });
+        }
+    });
+    res.redirect('/auth');
+}
+
+export { login, logout };
