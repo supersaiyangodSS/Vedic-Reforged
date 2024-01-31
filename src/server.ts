12b1e4da0e37eda30555f2bad3952a93c9987c06
vedantSSJ <81308923+supersaiyangodSS@users.noreply.github.com>
2024-01-31 23:03:51 +0530

    fix default app import

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import app from "./app.js";
+import { app } from "./app.js";
 const port = process.env.PORT || 4000;
 
 app.listen(port, () => console.log(`Server is up and running on port ${port}`));
