15aa4aa21b12810018801c085d935e029d81f7bd
vedantSSJ <81308923+supersaiyangodSS@users.noreply.github.com>
2024-02-01 23:21:49 +0530

    fix moved multer upload

--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,7 @@ import express , { Request, Response, Express, NextFunction } from 'express';
 import connectDB from './config/database.js';
 import main from './router/main.js';
 import auth from './router/auth.js';
-import multer from 'multer';
-import { fileURLToPath } from 'url';
-import { dirname, join } from 'path';
-import Sahity from './models/sahity.js';
+import uploader from './router/upload.js';
 import session from 'express-session';
 import MongoStore from 'connect-mongo';
 import crypto from 'crypto';
@@ -15,10 +12,6 @@ connectDB();
 
 const app : Express = express();
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
 const oneDay = 24 * 60 * 60 * 1000;
 const sessionSecret = crypto.randomBytes(64).toString('hex');
 
@@ -39,8 +32,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(sessions);
 
-
-
 function isAuth (req: Request, res: Response, next: NextFunction) {
     if (req.session && req.session.user) {
         next();
@@ -49,22 +40,9 @@ function isAuth (req: Request, res: Response, next: NextFunction) {
     }
 }
 
-app.post('/upload', isAuth, upload.single('jsonFile'), async (req: Request, res: Response) => {
-    try {
-        if (!req.file) {
-            return res.json({ error: 'No file uploaded!' });
-        }
-        const jsonData = JSON.parse(req.file.buffer.toString());
-        await Sahity.create(jsonData);
-        res.json({ success: 'file uploaded' });
-    } catch (error) {
-        console.log(error);
-        res.json(error) 
-    }
-})
-
 app.use('/api', main)
 app.use('/auth', auth)
+app.use('/upload', uploader)
 
-export default app;
+export {app, isAuth} ;
  
