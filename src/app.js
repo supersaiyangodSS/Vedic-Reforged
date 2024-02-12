56efc7c4da6fa23396b0cf7c57ec092d82d51bc8
vedantSSJ <81308923+supersaiyangodSS@users.noreply.github.com>
2024-02-12 23:55:13 +0530

    commit error vscode

--- src/app.js
+++ src/app.js
@@ -1,32 +1,10 @@
-import { config } from 'dotenv';
-config();
 import express from 'express';
-import cors from 'cors';
-import session from 'express-session';
-import MongoStore from 'connect-mongo';
-import crypto from 'crypto';
 
 const app = express();
 
-const oneDay = 24 * 60 * 60 * 1000;
-const sessionSecret = crypto.randomBytes(64).toString('hex');
-
-const sessions = session({
-    secret: process.env.SECRET || sessionSecret,
-    resave: false,
-    cookie: {
-        path: '/',
-        httpOnly: false,
-        maxAge: oneDay,
-    },
-    store: MongoStore.create({ mongoUrl: process.env.DB }),
-    saveUninitialized: true,
-})
-app.use(cors())
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(sessions);
 
 export {app};
  
