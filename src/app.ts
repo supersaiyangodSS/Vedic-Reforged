0e6c98ac150fcbb10c821270ca3326d9c67df428
vedantSSJ <81308923+supersaiyangodSS@users.noreply.github.com>
2024-02-17 23:49:06 +0530

    add dotenv config to the main app

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express, { Request, Response, Express } from 'express';
+import connectDB from './config/database.js';
+connectDB();
 
 const app : Express = express ();
 
 app.get('/', (req: Request, res: Response) => {
-    res.status(200).json({ test: "node" })
-})
+    res.status(200).json({ test: "node" });
+});
 
-export default app;
+export {app};
