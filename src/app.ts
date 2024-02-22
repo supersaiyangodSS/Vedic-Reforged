f8611b429f2ee3a083853312bb20b036a01f92c0
vedantSSJ <81308923+supersaiyangodSS@users.noreply.github.com>
2024-02-22 23:07:17 +0530

    ➕ add session to the main app

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,22 @@
 import express, { Request, Response, Express } from 'express';
 import connectDB from './config/database.js';
 import Auth_Router from './routes/auth.js';
+import session from 'express-session';
 connectDB();
 
 const app : Express = express ();
+
+const options = {
+    secret: 'test', //TODO:
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        maxAge: 24 * 60 * 60 * 1000,
+        secure: false
+    }
+}
+
+app.use(session(options));
 
 app.get('/', (req: Request, res: Response) => {
     res.status(200).json({ test: "node" });
