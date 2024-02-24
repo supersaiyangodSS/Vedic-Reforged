e2de572bbc515389828db159090fecf225215439
vedantSSJ <81308923+supersaiyangodSS@users.noreply.github.com>
2024-02-24 21:24:05 +0530

    ➕ add serve static, json, urlextended and cors to the main app

--- src/app.ts
+++ src/app.ts
@@ -2,21 +2,29 @@ import express, { Request, Response, Express } from 'express';
 import connectDB from './config/database.js';
 import Auth_Router from './routes/auth.js';
 import session from 'express-session';
+import cors from 'cors';
+import MongoStore from 'connect-mongo';
 connectDB();
 
 const app : Express = express ();
 
+const oneDay = 24 * 60 * 60 * 1000;
 const options = {
     secret: 'test', //TODO:
     resave: false,
     saveUninitialized: true,
     cookie: {
-        maxAge: 24 * 60 * 60 * 1000,
-        secure: false
-    }
+        path: '/',
+        httpOnly: false,
+        maxAge: oneDay,
+    },
+    store: MongoStore.create({ mongoUrl: process.env.DB }),
 }
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(session(options));
+app.use(cors());
+app.use(express.static('public'));
 
 app.get('/', (req: Request, res: Response) => {
     res.status(200).json({ test: "node" });
