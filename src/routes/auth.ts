6df0c7568c1bb22f0979c3c5a8d5b49054a63c80
vedantSSJ <81308923+supersaiyangodSS@users.noreply.github.com>
2024-02-28 16:05:53 +0530

    ➕ add POST: register user and validator

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,9 +1,21 @@
+import { registerUser } from '../controllers/authController.js';
 import { Router , Request, Response } from 'express';
+import { body } from 'express-validator';
 
 const router : Router = Router();
 
+const registerValidator = [
+    body('firstName').notEmpty().isString(),
+    body('lastName').notEmpty().isString(),
+    body('username').notEmpty().isAlphanumeric().isLength({ min: 3, max: 20 }),
+    body('email').notEmpty().isEmail(),
+    body('password').notEmpty().isLength({ min: 6 })
+]
+
 router.get('/', (req: Request, res: Response) => {
-    return res.json({ text: 'test' })
+    return res.json({ text: 'test' });
 });
+
+router.post('/user', registerValidator, registerUser);
 
 export default router;
