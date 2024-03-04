e6dfa65a86e257d35340fcefbf4fbbbedf12ff2d
vedantSSJ <81308923+supersaiyangodSS@users.noreply.github.com>
2024-03-04 14:42:34 +0530

    🔧 fix unused imports

--- src/models/Aarti.ts
+++ src/models/Aarti.ts
@@ -1,4 +1,4 @@
-import { Schema, model, Document } from 'mongoose';
+import { Schema, model } from 'mongoose';
 import { IAarti } from '../interface/AartiInterface.js';
 
 const AartiSchema = new Schema<IAarti>({
