f2fdeb004c7e9e6df5bf58b0a4aab44f6d048399
vedantSSJ <81308923+supersaiyangodSS@users.noreply.github.com>
2024-03-04 18:55:27 +0530

    ➕ integrated customErrorHandler

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,8 +1,9 @@
 import "express-session";
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import User from "../models/User.js";
 import { validationResult } from 'express-validator';
 import { compare } from 'bcrypt';
+import { CustomrError } from "../errors/customError.js";
 
 interface IRequest {
     firstName: string,
@@ -15,7 +16,7 @@ interface IRequest {
 
 enum Role { ADMIN, USER }
 
-const registerUser =  async (req: Request<{}, {}, IRequest>, res: Response) => {
+const registerUser =  async (req: Request<{}, {}, IRequest>, res: Response, next: NextFunction) => {
     const { firstName, lastName, email, username, password, address } = req.body;
     if (address) {
         return res.status(403).send('Access denied: Bot detection triggered.');
@@ -36,37 +37,46 @@ const registerUser =  async (req: Request<{}, {}, IRequest>, res: Response) => {
             await newUser.save();
             return res.status(200).send(`new user created successfully: ${newUser.firstName}`)
         } catch (error) {
-            console.log(error);        
+            console.log(error);
             return res.status(500).send("Internal Server Error: Unable to create user");
         }
     } catch (error) {
         console.log(error);
-        res.status(500).send("Internal Server Error!");
+        next(error);
     }
 }
 
-const loginUser = async (req: Request<{}, {}, IRequest>, res: Response) => {
+const loginUser = async (req: Request<{}, {}, IRequest>, res: Response, next: NextFunction) => {
     const { username, password, address } = req.body;
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        const errorOne = errors.array()[0].msg;
+
+        return res.status(301).redirect('/auth');
+    }
     if (address) {
         return res.status(403).send('Access denied: Bot detection triggered.');
     }
     try {
-        const findUser = await User.findOne({
-            username: username
-        })
+        const findUser = await User.findOne({username})
         if (!findUser) {
-            return res.status(404).send('Invalid username or password');
+            return res.status(401).send('Invalid username or password');
         }
         const matchPassword = await compare(password, findUser.password);
         if (!matchPassword) {
-            return res.status(404).send('Invalid username or password');
+            return res.status(401).send('Invalid username or password');
+        }
+        if (findUser.verified === false) {
+            res.status(301).redirect('/verify');
+            throw new CustomrError('Custom Error Message', 301);
         }
         req.session.user = findUser.username;
         req.session.role = findUser.role;
         req.session.uid = findUser._id;
+        return res.status(200).redirect('/dashboard');
     } catch (error) {
         console.log(error);
-        res.status(500).send("Internal Server Error!");
+        next(error);
     }
 }
 
