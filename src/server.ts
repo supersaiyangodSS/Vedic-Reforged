fad80db26dfb7fd1f036f8f11244f52d17297240
vedantSSJ <81308923+supersaiyangodSS@users.noreply.github.com>
2024-03-05 22:41:51 +0530

    ⚙️ add exposed localhost port to the network

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,4 @@ import {app} from './app.js';
 
 const port = 4000 || process.env.PORT;
 
-app.listen(port, () => console.log(`server running at port: ${port}`));
+app.listen(port, '0.0.0.0', () => console.log(`server running at port: ${port}`)); // ! remove local network
