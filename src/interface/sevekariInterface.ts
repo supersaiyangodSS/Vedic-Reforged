a9a058c6c2916d2a54f8740f2c16c23fce2ea65f
vedantSSJ <81308923+supersaiyangodSS@users.noreply.github.com>
2024-03-06 18:34:42 +0530

    ➕ add sevekari types interface

---
+++ src/interface/sevekariInterface.ts
@@ -0,0 +1,13 @@
+import { Document } from "mongoose";
+
+export declare interface ISevekari extends Document {
+    firstName: string,
+    middleName: string,
+    lastName: string,
+    gender: string[],
+    address: string,
+    mobile: number,
+    alternateMobile: number,
+    dateOfBirth: Date,
+    
+}
