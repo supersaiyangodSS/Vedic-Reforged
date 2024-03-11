f027e903adcedc4f9c2381ae9c1b956e0f1422e9
vedantSSJ <81308923+supersaiyangodSS@users.noreply.github.com>
2024-03-11 17:51:32 +0530

    ➕ add react-router to the app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,15 @@
-import RegisterForm from './components/login'
+import React from 'react';
+import LoginComponent from './components/login';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <RegisterForm />
+    <Router>
+      <Routes>
+        <Route path="/" exact element={<LoginComponent/>} />
+      </Routes> 
+    </Router>
   )
 }
 
-export default App
+export default App;
