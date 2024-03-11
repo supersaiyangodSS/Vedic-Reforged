d07a216a5efd7f79873a8e0644d851ee0f325c66
vedantSSJ <81308923+supersaiyangodSS@users.noreply.github.com>
2024-03-11 17:54:12 +0530

    ➖ remove express-session configuration

--- src/app.ts
+++ src/app.ts
@@ -3,38 +3,24 @@ config();
 import express, { Request, Response, Express } from 'express';
 import connectDB from './config/database.js';
 import AuthRouter from './routes/auth.js';
-import sessions from 'express-session';
 import cors from 'cors';
-import MongoStore from 'connect-mongo';
 import createAdmin from './routes/createAdmin.js';
+import auth from './auth/auth.js';
 connectDB();
 
 const app: Express = express ();
 
-const oneDay: number = 24 * 60 * 60 * 1000;
 const sessionSecret: string | undefined = process.env.SESSION_SECRET;
 if (!sessionSecret) {
     console.log('SESSION_SECRET environment variable is not defined.');
     process.exit(1);
 }
 
-const options = {
-    secret: sessionSecret,
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-        path: '/',
-        httpOnly: false,
-        maxAge: oneDay,
-    },
-    store: MongoStore.create({ mongoUrl: process.env.DB }),
-}
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(sessions(options));
 app.use(cors());
 app.use(express.static('public'));
-app.get('/', (req: Request, res: Response) => {
+app.get('/', auth, (req: Request, res: Response) => {
     res.status(200).json({ test: "node" });
 });
 
