adfbc736bcc55e3d904c77015165aa747018b7f8
vedantSSJ <81308923+supersaiyangodSS@users.noreply.github.com>
2024-04-19 21:21:08 +0530

    🗑️ remove dist

--- dist/routes/dashboard.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-const router = Router();
-router.get('/', (req, res) => {
-    res.render('dashboard', {
-        title: 'Shri Swami Samarth'
-    });
-});
-export default router;
-//# sourceMappingURL=dashboard.js.map
--- dist/server.js
+++
@@ -1,4 +0,0 @@
-import { app } from './app.js';
-const port = 4000 || process.env.PORT;
-app.listen(port, '0.0.0.0', () => console.log(`server running at port: ${port}`)); // ! remove local network
-//# sourceMappingURL=server.js.map
